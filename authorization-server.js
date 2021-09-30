325d4746e90a930f31954625627d9454940c5fb8
Gerardo Martinez <gerardo.mm@globant.com>
2021-09-30 14:20:03 -0500

    Finish Building the Authorization Server

--- authorization-server.js
+++ authorization-server.js
@@ -50,9 +50,70 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-Your code here
-*/
+app.get('/authorize', (req, res) => {
+	const { client_id, scope } = req.query
+	if (clients.hasOwnProperty(client_id) &&
+		containsAll(clients[client_id].scopes, scope.split(" "))) {
+		const requestId = randomString()
+		requests[requestId] = req.query
+		res.render("login",
+			{
+				client: clients[client_id],
+				scope,
+				requestId
+			})
+	} else {
+		res.status(401).end()
+	}
+})
+
+app.post("/approve", (req, res) => {
+	const { userName, password, requestId } = req.body
+
+	if (users[userName] === password && requests.hasOwnProperty(requestId)) {
+		const clientReq = requests[requestId]
+		delete requests[requestId]
+		const code = randomString()
+		authorizationCodes[code] = { clientReq, userName }
+		const { redirect_uri, state } = clientReq
+		const redirectUrl = new URL(redirect_uri)
+		redirectUrl.searchParams.append("code", code)
+		redirectUrl.searchParams.append("state", state)
+		res.redirect(redirectUrl)
+	} else {
+		res.status(401).end()
+	}
+})
+
+app.post('/token', (req, res) => {
+	if (req.headers.hasOwnProperty("authorization")) {
+		const { clientId, clientSecret } = decodeAuthCredentials(req.headers.authorization)
+		if (clients[clientId].clientSecret === clientSecret) {
+			const { code } = req.body
+			if (authorizationCodes.hasOwnProperty(code)) {
+				const authorizationCode = authorizationCodes[code]
+				delete authorizationCodes[code]
+				const token = jwt.sign(
+					{
+						userName: authorizationCode.userName,
+						scope: authorizationCode.clientReq.scope
+					}, config.privateKey, { algorithm: "RS256" })
+				res.json(
+					{
+						"access_token": token,
+						"token_type": "Bearer"
+					}
+				)
+			} else {
+				res.status(401).end()
+			}
+		} else {
+			res.status(401).end()
+		}
+	} else {
+		res.status(401).end()
+	}
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
