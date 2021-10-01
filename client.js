bdfe93e1b49ca8e819b6db5d82ce2a2dc63a3cc7
Gerardo Martinez <gerardo.mm@globant.com>
2021-10-01 16:37:59 -0500

    Finish Building the Client Application

--- client.js
+++ client.js
@@ -14,6 +14,7 @@ const config = {
 	tokenEndpoint: "http://localhost:9001/token",
 	userInfoEndpoint: "http://localhost:9002/user-info",
 }
+
 let state = ""
 
 const app = express()
@@ -23,9 +24,41 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-Your code here
-*/
+app.get("/authorize", (res, req) => {
+	state = randomString()
+	const redirectUrl = new URL(config.authorizationEndpoint)
+	redirectUrl.searchParams.append("response_type", "code")
+	redirectUrl.searchParams.append("client_id", config.clientId)
+	redirectUrl.searchParams.append("redirect_uri", config.redirectUri)
+	redirectUrl.searchParams.append("scope", "permission:name permission:date_of_birth")
+	redirectUrl.searchParams.append("state", state)
+
+	req.redirect(redirectUrl)
+})
+
+app.get("/callback", (req, res) => {
+	if (req.query.state !== state) {
+		res.status(403).end()
+		return
+	}
+
+	axios({
+		method: "POST",
+		url: config.tokenEndpoint,
+		auth: { username: config.clientId, password: config.clientSecret },
+		data: { code: req.query.code }
+	})
+		.then(response => {
+			return axios({
+				method: "GET",
+				url: config.userInfoEndpoint,
+				headers: { authorization: `bearer ${response.data.access_token}` }
+			})
+		})
+		.then(response => {
+			res.render("welcome", { user: response.data })
+		})
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
